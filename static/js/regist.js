050c2d8ece3c8faa3ee94151232b686ef4b725af
lip <li@playab.net>
2013-07-30 11:04:47 +0800

    jsmart test

--- static/js/regist.js
+++ static/js/regist.js
@@ -135,6 +135,32 @@ $(document).ready(function(){
                     }
     var html = template(collection);
     $('#userList').html(html);
+
+
+    /*************************************  jSmart ****************************************/
+    var JsmartData = {
+       greeting: 'Hi, there are some JScript books you may find interesting:',
+       books: [
+          {
+             title  : 'JavaScript: The Definitive Guide',          
+             author : 'David Flanagan',                            
+             price  : '31.18'
+          },
+          {
+             title  : 'Murach JavaScript and DOM Scripting',
+             author : 'Ray Harris'
+          },
+          {
+             title  : 'Head First JavaScript',
+             author : 'Michael Morrison',
+             price  : '29.54'
+          }
+        ]      
+    };
+
+    var tpl = new jSmart($('#jsmartTestTpl').html());
+    var html = tpl.fetch(JsmartData);
+    $('#jsmartDiv').html(html);
 });
 
 
